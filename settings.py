85e0dfaa358dd53d0dea4a0d7de72987cdf86df2
Tadhg Xu-Glassop <t.xuglassop@gmail.com>
2024-09-13 01:33:31 +1000

    Added a default figure size option

--- settings.py
+++ settings.py
@@ -1,5 +1,4 @@
-# for each of the environments, 
-
+# colours for each of the callouts
 lemma = "lightgray"
 theorem = "black"
 definition = "violet"
@@ -8,3 +7,6 @@ remark = "Dandelion"
 note = "blue"
 info = "Aquamarine"
 warning = "orange"
+
+# default figure size
+figure_size = "7cm"
