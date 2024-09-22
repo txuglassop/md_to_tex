6bfb60fbd30526c0edb6847bcaa2055c546e32af
Tadhg Xu-Glassop <t.xuglassop@gmail.com>
2024-09-22 12:29:49 +1000

    Updated bash script to allow passing a directory to md files option. Defaults to current wd if no argument is provided

--- start.sh
+++ start.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-python3 main.py
+# get current directory
+SCRIPT_DIR="$(dirname "$0")"
+
+if [ -z "$1" ]; then
+    # pass current wd as default directory
+    echo "No directory provided. Using script directory."
+    python3 "$SCRIPT_DIR/main.py" "$SCRIPT_DIR"
+else 
+    # pass provided path to main.py
+    echo "Using provided directory: $1"
+    python3 "$SCRIPT_DIR/main.py" "$1"
+fi
